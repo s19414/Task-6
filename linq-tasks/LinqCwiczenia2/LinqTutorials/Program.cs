6ba3f85b1aeddc5e063627d0d9fcf2ffb5d82cae
s19414 <s19414@pjwstk.edu.pl>
2023-02-10 03:42:47 +0100

    added task 6

---
+++ linq-tasks/LinqCwiczenia2/LinqTutorials/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 1: " + LinqTasks.Task1().ToString());
+
+        }
+
+    }
+}
